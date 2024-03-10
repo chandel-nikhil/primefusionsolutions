984e5152caf367b2b95325321b85006f5042aa70
chandel-nikhil <chandel.nikhil97@gmail.com>
2024-03-10 23:23:45 +0530

    theme update

--- themes/twentytwetyfour-child/header.php
+++ themes/twentytwetyfour-child/header.php
@@ -8,23 +8,68 @@
     <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
-    <header>
-        <div class="site-branding">
+<?php get_header(); ?>
+
+<body class="sub_page" <?php body_class(); ?>>
+
+    <div class="hero_area">
+
+        <div class="hero_bg_box">
+            <div class="bg_img_box">
+                <img src="images/hero-bg.png" alt="">
+            </div>
+        </div>
+
+        <!-- header section strats -->
+        <header class="header_section">
             <?php if (has_custom_logo()) : ?>
                 <div class="site-logo"><?php the_custom_logo(); ?></div>
             <?php else : ?>
                 <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
                 <p class="site-description"><?php bloginfo('description'); ?></p>
             <?php endif; ?>
-        </div>
-        <nav class="site-navigation">
-            <?php
-            wp_nav_menu(array(
-                'theme_location' => 'primary',
-                'menu_id' => 'primary-menu',
-            ));
-            ?>
-        </nav>
-    </header>
-    <?php get_header(); ?>
+            <div class="container-fluid">
+                <nav class="navbar navbar-expand-lg custom_nav-container ">
+                    <?php
+                    wp_nav_menu(array(
+                        'theme_location' => 'primary',
+                        'menu_id' => 'primary-menu',
+                    ));
+                    ?>
+
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class=""> </span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav  ">
+                            <li class="nav-item ">
+                                <a class="nav-link" href="index.html">Home </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="about.html"> About</a>
+                            </li>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="service.html">Services <span class="sr-only">(current)</span> </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="why.html">Why Us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="team.html">Team</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"> <i class="fa fa-user" aria-hidden="true"></i> Login</a>
+                            </li>
+                            <form class="form-inline">
+                                <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
+                                    <i class="fa fa-search" aria-hidden="true"></i>
+                                </button>
+                            </form>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <!-- end header section -->
+    </div>
